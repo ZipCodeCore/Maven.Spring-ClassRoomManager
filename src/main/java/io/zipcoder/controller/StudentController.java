f6f0b905c4c3dd41630ebbadd96afe8e02540c5b
Tariq Hook <thook@tariqs-mbp-2.wifi.sevone.com>
2016-06-01 13:43:25 -0400

    Updates

--- src/main/java/io/zipcoder/controller/StudentController.java
+++ src/main/java/io/zipcoder/controller/StudentController.java
@@ -3,11 +3,15 @@ package io.zipcoder.controller;
 import io.zipcoder.domain.Student;
 import io.zipcoder.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 public class StudentController {
@@ -21,4 +25,18 @@ public class StudentController {
         return new ResponseEntity<>(allStudents, HttpStatus.OK);
     }
 
+    @RequestMapping(value="/student", method = RequestMethod.POST)
+    public ResponseEntity<?> createStudent(){
+        Student student = new Student();
+        student.setFirstName("Chuck");
+        student.setId(22l);
+        student = studentRepository.save(student);
+
+        // Set the location header for the newly created resource
+        HttpHeaders responseHeaders = new HttpHeaders();
+        URI newPollUri = ServletUriComponentsBuilder.fromCurrentRequest().path("/students").buildAndExpand(student.getId()).toUri();
+        responseHeaders.setLocation(newPollUri);
+
+        return new ResponseEntity<>(null, responseHeaders, HttpStatus.CREATED);
+    }
 }
